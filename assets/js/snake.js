7e7a670d60d9dc9a7fe0f0d1dc1cb6b637eaa310
Abner <username@DESKTOP-IIMOBTI.localdomain>
2021-08-14 00:33:55 +0800

    fix copy right below not visible

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -8,9 +8,6 @@ function handleIndicator(el) {
     item.classList.remove('is-active');
     item.removeAttribute('style');
   });
-  indicator.style.width = "".concat(el.offsetWidth, "px");
-  indicator.style.left = "".concat(el.offsetLeft, "px");
-  indicator.style.backgroundColor = el.getAttribute('active-color');
   el.classList.add('is-active');
   el.style.color = el.getAttribute('active-color');
 }
